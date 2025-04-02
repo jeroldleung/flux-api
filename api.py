449ac5aa069e7b41920e885059eb238270412638
jeroldleung <jeroldleung@gmail.com>
2025-04-02 22:34:09 +0800

    providing an api of generating an image

--- api.py
+++ api.py
@@ -1,21 +1,25 @@
 import argparse
+import io
 
 import torch
 import uvicorn
-from diffusers import FluxPipeline
+from diffusers import BitsAndBytesConfig as DiffusersBitsAndBytesConfig
+from diffusers import FluxPipeline, FluxTransformer2DModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, Field
 from scalar_fastapi import get_scalar_api_reference
+from transformers import BitsAndBytesConfig as TransformersBitsAndBytesConfig
+from transformers import T5EncoderModel
 
 
 class Request(BaseModel):
     prompt: str = Field("A cat holding a sign that says hello world", description="Text prompt for image generation.")
     width: int = Field(32, ge=16, le=1024, description="Width of the generated image in pixels. Must can be devided by 16.")
     height: int = Field(32, ge=16, le=1024, description="Height of the generated image in pixels. Must can be devided by 16.")
-    steps: int | None = Field(4, ge=1, le=50, description="Number of steps for the image generation process.")
-    guidance: int | None = Field(0.0, description="High guidance scales improve prompt adherence but reduce realism.")
+    num_inference_steps: int | None = Field(4, ge=1, le=50, description="Number of steps for the image generation process.")
+    guidance_scale: int | None = Field(0.0, description="High guidance scales improve prompt adherence but reduce realism.")
 
 
 api = FastAPI(title="flux-api", docs_url=None, redoc_url=None)
@@ -30,15 +34,11 @@ async def scalar_html():
 
 @api.post("/v1/inference")
 async def generate_image(req: Request):
-    out = pipe(
-        prompt=req.prompt,
-        width=req.width,
-        height=req.height,
-        num_inference_steps=req.steps,
-        guidance_scale=req.guidance,
-    ).images[0]
-
-    return FileResponse(out.save("result.png"), media_type="image/png")
+    out = pipe(**req.model_validate_json()).images[0]
+    imageb = io.BytesIO()
+    out.save(imageb, format="PNG")
+    imageb.seek(0)
+    return StreamingResponse(imageb, media_type="image/png")
 
 
 if __name__ == "__main__":
@@ -47,7 +47,31 @@ if __name__ == "__main__":
     parser.add_argument("--path", type=str, default="pretrained_models/FLUX.1-dev")
     args = parser.parse_args()
 
-    pipe = FluxPipeline.from_pretrained(args.path, torch_dtype=torch.bfloat16)
-    pipe.to("cuda")
+    # quantizing the flux in 4-bit
+
+    quant_config = TransformersBitsAndBytesConfig(load_in_4bit=True)
+    text_encoder_2_4bit = T5EncoderModel.from_pretrained(
+        args.path,
+        subfolder="text_encoder_2",
+        quantization_config=quant_config,
+        torch_dtype=torch.bfloat16,
+    )
+
+    quant_config = DiffusersBitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
+    transformer_4bit = FluxTransformer2DModel.from_pretrained(
+        args.path,
+        subfolder="transformer",
+        quantization_config=quant_config,
+        torch_dtype=torch.bfloat16,
+    )
+
+    pipe = FluxPipeline.from_pretrained(
+        args.path,
+        transformer=transformer_4bit,
+        text_encoder_2=text_encoder_2_4bit,
+        torch_dtype=torch.bfloat16,
+    )
+
+    pipe.to("cuda")  # load model to gpu
 
     uvicorn.run(api, host="0.0.0.0", port=args.port)
